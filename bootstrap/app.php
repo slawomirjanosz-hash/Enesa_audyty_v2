816cab61d4a6271cdb278ed140143ebdbd6e9aae
Sławomir Janosz <D:\The Bat!>
2026-06-04 11:10:24 +0200

    feat: trust proxies w bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
         ]);
