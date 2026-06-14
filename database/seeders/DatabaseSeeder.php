b1895bbb364eb4ce40f59156a651eb4a35eeaeb6
Sławomir Janosz <D:\The Bat!>
2026-06-14 19:15:54 +0200

    fix: seed required roles from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+            AddSeniorAuditorRoleSeeder::class,
+            SuperAdminSeeder::class,
+        ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
