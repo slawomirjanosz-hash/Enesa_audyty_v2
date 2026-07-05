1fb8b0573a99306f4834e391654fec2720974e05
Sławomir Janosz <D:\The Bat!>
2026-07-05 22:33:33 +0200

    feat: add text_sections to Offer model fillable and casts

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -35,6 +35,7 @@ class Offer extends Model
         'content_payment',
         'show_unit_prices',
         'price_sections',
+        'text_sections',
         'delegations',
         'is_template',
     ];
@@ -44,6 +45,7 @@ class Offer extends Model
         'valid_until'       => 'date',
         'show_unit_prices'  => 'boolean',
         'price_sections'    => 'array',
+        'text_sections'     => 'array',
         'delegations'       => 'array',
         'is_template'       => 'boolean',
     ];
