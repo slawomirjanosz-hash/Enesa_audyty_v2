7e6672a36e0ccbf5539d1b6169582a7f3c7323e2
Sławomir Janosz <D:\The Bat!>
2026-07-06 15:18:47 +0200

    feat: add TaskOverdue mailable

---
+++ app/Mail/TaskOverdue.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+
+class TaskOverdue extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(public Collection $tasks) {}
+
+    public function build()
+    {
+        return $this->subject('Masz ' . $this->tasks->count() . ' zaległych zadań w ENESA CRM')
+            ->view('emails.task-overdue')
+            ->with(['tasks' => $this->tasks]);
+    }
+}
