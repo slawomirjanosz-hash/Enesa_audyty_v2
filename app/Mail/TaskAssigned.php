26931e4f01bc37d88b339e1611e1de6989581eec
Sławomir Janosz <D:\The Bat!>
2026-07-06 15:17:14 +0200

    feat: add TaskAssigned mailable

---
+++ app/Mail/TaskAssigned.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Task;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TaskAssigned extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(public Task $task) {}
+
+    public function build()
+    {
+        return $this->subject('Nowe zadanie: ' . $this->task->title)
+            ->view('emails.task-assigned')
+            ->with(['task' => $this->task]);
+    }
+}
