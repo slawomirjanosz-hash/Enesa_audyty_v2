22cc7adc79b955620b01a4c1fe0c3087f0588cc0
Sławomir Janosz <D:\The Bat!>
2026-08-06 08:24:46 +0200

    Verify soft-deleted users are excluded from queries

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -64,7 +64,8 @@ test('user can delete their account', function () {
         ->assertRedirect('/');
 
     $this->assertGuest();
-    $this->assertNull($user->fresh());
+    $this->assertSoftDeleted($user);
+    $this->assertNull(User::find($user->id));
 });
 
 test('correct password must be provided to delete account', function () {
